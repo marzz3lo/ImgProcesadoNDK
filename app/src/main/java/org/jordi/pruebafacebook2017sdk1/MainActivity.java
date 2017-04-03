ccbd7bf80e06137b3abff66b57d2167fefbf5677
marzz3lo <marzzelo@gmail.com>
2017-04-02 23:18:24 -0400

    Adding facebook login

--- app/src/main/java/org/jordi/pruebafacebook2017sdk1/MainActivity.java
+++ app/src/main/java/org/jordi/pruebafacebook2017sdk1/MainActivity.java
@@ -1,8 +1,12 @@
 package org.jordi.pruebafacebook2017sdk1;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +15,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.Profile;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +37,11 @@ public class MainActivity extends AppCompatActivity {
     private Bitmap bitmapMarco1 = null;
     private Bitmap bitmapMarco2 = null;
     private ImageView ivDisplay = null;
+
+
+    LoginButton loginButtonOficial;
+    private CallbackManager elCallbackManagerDeFacebook;
+    private final Activity THIS = this;
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
@@ -37,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
     public native void crearMarco2(Bitmap bitmapIn, Bitmap bitmapOut);
 
     public static boolean hayPixel(int x, int y) {
-        return x%10 == y%10;
+        return x % 10 == y % 10;
     }
 
     @Override
@@ -46,18 +67,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.i(tag, "Imagen antes de modificar");
         ivDisplay = (ImageView) findViewById(R.id.ivDisplay);
-//        BitmapFactory.Options options = new BitmapFactory.Options(); // Asegurar que la imagen tiene 24 bits de color
-//        options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-//        bitmapOriginal = BitmapFactory.decodeResource(this.getResources(), R.drawable.sampleimage, options);
-//        if (bitmapOriginal != null) ivDisplay.setImageBitmap(bitmapOriginal);
+
+        loginButtonOficial = (LoginButton) findViewById(R.id.login_button);
+        loginButtonOficial.setPublishPermissions("publish_actions");
+
+        this.elCallbackManagerDeFacebook = CallbackManager.Factory.create();
+
+        LoginManager.getInstance().registerCallback(this.elCallbackManagerDeFacebook,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Toast.makeText(THIS, "Login onSuccess()", Toast.LENGTH_LONG).show();
+                        actualizarVentanita();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Toast.makeText(THIS, "Login onCancel()", Toast.LENGTH_LONG).show();
+                        actualizarVentanita();
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Toast.makeText(THIS, "Login onError(): " + exception.getMessage(),
+                                Toast.LENGTH_LONG).show();
+                        actualizarVentanita();
+                    }
+                });
+
+        this.actualizarVentanita();
+
     }
 
-    public void ResetImagen (){
+
+
+    public void ResetImagen() {
         Log.i(tag, "Resetear Imagen");
         ivDisplay.setImageBitmap(bitmapOriginal);
     }
 
-    public void ConvertirGrises (){
+    public void ConvertirGrises() {
         Log.i(tag, "Conversion a escala de grises");
         bitmapGrises = Bitmap.createBitmap(bitmapOriginal.getWidth(), bitmapOriginal.getHeight(), Bitmap.Config.ARGB_8888);
         convertirGrises(bitmapOriginal, bitmapGrises);
@@ -71,19 +120,20 @@ public class MainActivity extends AppCompatActivity {
         ivDisplay.setImageBitmap(bitmapSepia);
     }
 
-    public void CrearMarco1 (){
+    public void CrearMarco1() {
         Log.i(tag, "Creando Marco 1");
         bitmapMarco1 = Bitmap.createBitmap(bitmapOriginal.getWidth(), bitmapOriginal.getHeight(), Bitmap.Config.ARGB_8888);
         crearMarco1(bitmapOriginal, bitmapMarco1);
         ivDisplay.setImageBitmap(bitmapMarco1);
     }
 
-    public void CrearMarco2 (){
+    public void CrearMarco2() {
         Log.i(tag, "Creando Marco 2");
         bitmapMarco2 = Bitmap.createBitmap(bitmapOriginal.getWidth(), bitmapOriginal.getHeight(), Bitmap.Config.ARGB_8888);
         crearMarco2(bitmapOriginal, bitmapMarco2);
         ivDisplay.setImageBitmap(bitmapMarco2);
     }
+
     public void onSacarFoto(View v) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
@@ -100,7 +150,69 @@ public class MainActivity extends AppCompatActivity {
             if (bitmapOriginal != null)
                 ivDisplay.setImageBitmap(bitmapOriginal);
         }
+
+        this.elCallbackManagerDeFacebook.onActivityResult(requestCode, resultCode, data);
     }
+
+    private void actualizarVentanita()
+    {
+        AccessToken accessToken = this.obtenerAccessToken();
+
+        if (accessToken== null)
+        {
+
+        }
+//
+// sí hay sesión
+//
+        Log.d("cuandrav.actualizarVent", "hay sesion habilito");
+        //this.botonHacerLogin.setEnabled(false);
+//        this.botonLogOut.setEnabled(true);
+//        this.textoConElMensaje.setEnabled(true);
+//        this.botonCompartir.setEnabled(true);
+//        this.botonEnviarFoto.setEnabled(true);
+//
+// averiguo los datos básicos del usuario acreditado
+//
+        Profile profile = Profile.getCurrentProfile();
+        if (profile != null) {
+//            this.textoConElMensaje.setText(profile.getName());
+        }
+    }
+
+    private boolean sePuedePublicar() {
+        if (!this.hayRed()) {
+            Toast.makeText(this, "¿no hay red? No puedo publicar", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        if (! this.tengoPermisoParaPublicar()) {
+            Toast.makeText(this, "¿no tengo permisos para publicar? Los pido.", Toast.LENGTH_LONG).show();
+            LoginManager.getInstance().logInWithPublishPermissions(this, Arrays.asList("publish_actions"));
+            return false;
+        }
+        return true;
+    }
+
+    private boolean tengoPermisoParaPublicar() {
+        AccessToken accessToken = this.obtenerAccessToken();
+        return accessToken != null && accessToken.getPermissions().contains("publish_actions");
+    }
+
+
+    private boolean hayRed() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager
+                .getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+
+    private AccessToken obtenerAccessToken() {
+        return AccessToken.getCurrentAccessToken();
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -112,28 +224,23 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.original)
-        {
+        if (id == R.id.original) {
             ResetImagen();
         }
 
-        if (id == R.id.grises)
-        {
+        if (id == R.id.grises) {
             ConvertirGrises();
         }
 
-        if (id == R.id.sepia)
-        {
+        if (id == R.id.sepia) {
             ConvertirSepia();
         }
 
-        if (id == R.id.marco1)
-        {
+        if (id == R.id.marco1) {
             CrearMarco1();
         }
 
-        if (id == R.id.marco2)
-        {
+        if (id == R.id.marco2) {
             CrearMarco2();
         }
 
